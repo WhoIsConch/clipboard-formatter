684be995ce102a75bedc129a0f3eae33c8b5c639
whoisconch <conch@twisea.net>
2024-04-19 08:06:50 -0400

    Add MAC formatter

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use clipboard_win::{monitor, formats, get_clipboard, set_clipboard};
+use clipboard_win::{monitor, formats, get_clipboard, set_clipboard, ErrorCode};
 use regex::Regex;
 
 fn parse_phone_number(input: &str) -> Option<String> {
-    let already_parsed_reg = Regex::new(r"\b\d{10}\b").unwrap();
+    let already_formatted_reg = Regex::new(r"\b\d{10}\b").unwrap();
 
-    if already_parsed_reg.is_match(input) {
+    if already_formatted_reg.is_match(&input) {
         return None;
     }
 
-    let reg = Regex::new(r"\b(?:\(?\d{3}\)?\s?\d{3}[\-\s]?\d{4}|\d{10})\b").unwrap();
-    let captures = reg.captures(input)?;
+    let phone_reg = Regex::new(r"\b(?:\(?\d{3}\)?\s?\d{3}[\-\s]?\d{4}|\d{10})\b").unwrap();
+    let captures = phone_reg.captures(&input)?;
     let phone_number = captures
         .get(0)
         .map(|m| m.as_str())
@@ -25,20 +25,72 @@ fn parse_phone_number(input: &str) -> Option<String> {
     }
 }
 
+fn parse_mac_address(input: &str) -> Option<String> {
+    let already_formatted_reg = Regex::new(r"^([0-9A-Fa-f]{4}\.)([0-9A-Fa-f]{4}\.)([0-9A-Fa-f]{4})$").unwrap();
+
+    if already_formatted_reg.is_match(&input) {
+        return None;
+    }
+
+    let mac_reg = Regex::new(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$").unwrap();
+    let captures = mac_reg.captures(&input)?;
+    let mac_addr = captures
+        .get(0)
+        .map(|m| m.as_str())
+        .unwrap_or_default()
+        .chars()
+        .filter(|c| c.is_ascii_hexdigit())
+        .enumerate()
+        .fold(String::new(), |mut acc, (idx, c)| {
+            acc.push(c);
+            if idx % 4 == 3 && idx != 11 {
+                acc.push('.');
+            }
+            acc
+        });
+
+    if mac_addr.len() == 14 {
+        Some(mac_addr)
+    } else {
+        None
+    }
+
+}
+
 fn main() {
     let mut mon = monitor::Monitor::new().unwrap();
     
     loop {
         let _ = mon.recv();
 
-        let res: String = get_clipboard(formats::Unicode).expect("Could not read clipboard");
+        let res: Result<String, ErrorCode> = get_clipboard(formats::Unicode);
 
-        dbg!(&res);
+        let copied = match res {
+            Err(error) => {
+                eprintln!("Failed to get clipboard data: {error}.");
+                continue;
+            },
+            Ok(result) => {
+                result
+            }
+        };
 
-        let phone = parse_phone_number(&res);
+        let copied = copied.trim();
+
+        let phone = parse_phone_number(&copied);
 
         if let Some(parsed_num) = phone {
-            let _ = set_clipboard(formats::Unicode, parsed_num);
+            let _ = set_clipboard(formats::Unicode, &parsed_num);
+            println!("Copied {parsed_num} to clipboard.");
+            continue;
+        } 
+
+        let mac = parse_mac_address(&copied);
+
+        if let Some(parsed_mac) = mac {
+            let _ = set_clipboard(formats::Unicode, &parsed_mac);
+            println!("Copied {parsed_mac} to clipboard.");
+            continue;
         }
 
     }
